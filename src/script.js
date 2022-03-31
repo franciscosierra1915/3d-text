fb2a779cdc2e6bc21d5173667087d82a2b9408ae
Francisco Sierra <64598498+franciscosierra1915@users.noreply.github.com>
2022-03-31 10:00:48 -0500

    Donuts working

--- src/script.js
+++ src/script.js
@@ -22,6 +22,8 @@ const scene = new THREE.Scene();
  * Textures
  */
 const textureLoader = new THREE.TextureLoader();
+const matcapTexture = textureLoader.load("/textures/matcaps/9.png");
+const material = new THREE.MeshMatcapMaterial({ matcap: matcapTexture });
 
 /**
  * Fonts
@@ -29,10 +31,10 @@ const textureLoader = new THREE.TextureLoader();
 const fontLoader = new FontLoader();
 
 fontLoader.load("/fonts/helvetiker_regular.typeface.json", (font) => {
-  const textGeometry = new TextGeometry("Hello Three.js", {
+  const textGeometry = new TextGeometry("Voodoo Doughnut", {
     font: font,
-    size: 0.5,
-    height: 0.2,
+    size: 0.3,
+    height: 0.1,
     curveSegments: 12,
     bevelEnabled: true,
     bevelThickness: 0.03,
@@ -40,17 +42,27 @@ fontLoader.load("/fonts/helvetiker_regular.typeface.json", (font) => {
     bevelOffset: 0,
     bevelSegments: 5,
   });
-  const textMaterial = new THREE.MeshBasicMaterial({ wireframe: true });
-  textGeometry.computeBoundingBox()
-  textGeometry.translate(
-    - (textGeometry.boundingBox.max.x - 0.02) * 0.5, // Subtract bevel size
-    - (textGeometry.boundingBox.max.y - 0.02) * 0.5, // Subtract bevel size
-    - (textGeometry.boundingBox.max.z - 0.03) * 0.5  // Subtract bevel thickness
-)
-  const text = new THREE.Mesh(textGeometry, textMaterial);
+  
+  textGeometry.center();
+  const text = new THREE.Mesh(textGeometry, material);
   scene.add(text);
+  //Donuts
+  const donutGeometry = new THREE.TorusGeometry(0.3, 0.2, 20, 45);
+  for (let i = 0; i < 100; i++) {
+    const donut = new THREE.Mesh(donutGeometry, material);
+    donut.position.x = (Math.random() - 0.5) * 10;
+    donut.position.y = (Math.random() - 0.5) * 10;
+    donut.position.z = (Math.random() - 0.5) * 10;
+    console.log("x;", donut.position.x)
+    console.log("y;", donut.position.y)
+    console.log("z;", donut.position.z)
+    // donut.rotation.x = Math.random() * Math.PI;
+    // donut.rotation.y = Math.random() * Math.PI;
+    // const scale = Math.random();
+    // donut.scale.set(scale, scale, scale);
+    scene.add(donut);
+  }
 });
-
 
 /**
  * Sizes
